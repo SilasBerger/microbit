c944fbceafc39247389edf36cc68ec3bad6fcadd
Silas Berger <silas.berger@outlook.com>
2023-12-10 15:03:25 +0100

    Add basic example in Python and makecode.

---
+++ examples/microbit/basic-shake-and-button-press.py
@@ -0,0 +1,20 @@
+# ----------------------------------------------------------------------------------------
+# Shake the Microbit to show the skull logo and play a sound. Press button A to reset.
+# See also: makecode-basic-shake-and-button-press.md
+# ----------------------------------------------------------------------------------------
+
+# Imports go at the top
+from microbit import *
+
+# Show heart image on start
+display.show(Image.HEART)
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    if accelerometer.was_gesture('shake'):
+        music.set_tempo(bpm=80)
+        music.play(['b3:1', 'f4:1', ':1', 'f4:1', 'f4:1', 'e4:1', 'd4:1', 'c4:3'])
+        display.show(Image.SKULL)
+
+    if button_a.was_pressed():
+        display.show(Image.HEART)
